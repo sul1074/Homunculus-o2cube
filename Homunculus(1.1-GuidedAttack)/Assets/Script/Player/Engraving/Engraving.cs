33ff81fd6c9d3eb05ca1a92e5ff714de210703c8
kimantt <ee2125@naver.com>
2024-01-27 18:11:44 +0900

    각인 시스템 추가
    - 'L' 버튼으로 키고 끌 수 있음
    - 마우스 오버 중일때 툴팁 보이는 것은 추가 예정
    - 추가되는 능력치는 Engraving.cs 스크립트의 getter 메소드로 불러올 수 있음

---
+++ Homunculus(1.1-GuidedAttack)/Assets/Script/Player/Engraving/Engraving.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engraving : MonoBehaviour
+{
+    private bool visible;
+    private GameObject EngravingPanel;
+
+    private int numOfButtons;
+    public EngravingButton[] Buttons;
+
+    private float tatalMaxHp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        visible = false;
+        EngravingPanel = transform.Find("Panel").gameObject;
+        numOfButtons = Buttons.Length;
+
+        tatalMaxHp = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            openAndClosePanel();
+        }
+    }
+
+    public void openAndClosePanel()
+    {
+        if (visible == false)
+            visible = true;
+        else if (visible == true)
+            visible = false;
+
+        EngravingPanel.SetActive(visible);
+    }
+
+    public float getToTalMaxHp() { return this.tatalMaxHp; }  // getter
+    public void setToTalMaxHp()
+    {
+        tatalMaxHp = 0;
+
+        for (int i = 0; i < numOfButtons; i++)
+        {
+            if (Buttons[i].actived == true)
+                tatalMaxHp += Buttons[i].hpStatus;
+        }
+    }
+}
---
+++ Homunculus(1.1-GuidedAttack)/Assets/Script/Player/Engraving/EngravingButton.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EngravingButton : MonoBehaviour
+{
+    public Engraving engravingSystem;
+    public EngravingButton parent;
+    private int numOfChild;
+    public EngravingButton[] child;
+    private Image image;
+    public Text text;
+    public bool actived;
+    public bool actable;
+
+    public float hpStatus;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+
+        numOfChild = child.Length;
+
+        if (parent)
+            actable = parent.actived;
+        else
+            actable = true;
+        actived = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckActable();
+
+        if (actived == true)
+        {
+            image.color = new Color(1, 1, 1, 1);
+            text.color = new Color(0, 0, 0, 1);
+        }
+        else if (actived == false && actable == true)
+        {
+            image.color = new Color(0.5f, 0.5f, 0.5f, 1);
+            text.color = new Color(1, 1, 1, 1);
+        }
+        else if (actived == false && actable == false)
+        {
+            image.color = new Color(0, 0, 0, 1);
+            text.color = new Color(1, 1, 1, 1);
+        }
+    }
+
+    public void OnClick()
+    {
+        if (actived == false && actable == true)
+        {
+            actived = true;
+        }
+        else if (actived == true)
+        {
+            UnactivateChild();
+        }
+
+        engravingSystem.setToTalMaxHp();
+    }
+
+    private void CheckActable()
+    {
+        if (parent)
+        {
+            if (parent.actived == false)
+            {
+                this.actived = false;
+                this.actable = false;
+            }
+            else if (parent.actived == true)
+            {
+                this.actable = true;
+            }
+        }
+    }
+
+    private void UnactivateChild()
+    {
+        this.actived = false;
+        if (numOfChild > 0)
+        {
+            for (int i = 0; i < numOfChild; i++)
+                child[i].UnactivateChild();
+        }
+    }
+}
