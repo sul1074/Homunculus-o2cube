fcb56463afa23e8225e2d7a121e083ed583868b3
kimantt <ee2125@naver.com>
2024-02-01 03:03:25 +0900

    몬스터가 벽을 타고 올라가는 문제 해결
    - 첫번째 방 바닥에 몬스터 점프 테스트용 발판을 설치해뒀습니다. 테스트 해보시고 이제 필요없겠다 싶으시면 없애주세요

--- Homunculus(1.1-GuidedAttack)/Assets/Script/Enemy/EnemyController.cs
+++ Homunculus(1.1-GuidedAttack)/Assets/Script/Enemy/EnemyController.cs
@@ -21,6 +21,8 @@ public class EnemyController : MonoBehaviour
     private bool isRandomVelocityFixed = false;
     private int switchvalue = 0;
 
+    private int jumpTime = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,17 +36,24 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left* randomVelocity * Time.deltaTime);
+        if (!((Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.63f), Vector2.left * randomVelocity, 0.7f, LayerMask.GetMask("Platform"))) || (Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.63f), Vector2.left * randomVelocity, 0.7f, LayerMask.GetMask("Wall")))))
+            transform.Translate(Vector2.left * randomVelocity * Time.deltaTime);
 
-        Vector2 frontVec = new Vector2(transform.position.x - randomVelocity, transform.position.y);
-        Debug.DrawRay(frontVec, Vector2.down*4f, new Color(0, 1, 0));
-        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector2.down*4f, 1, LayerMask.GetMask("Platform"));
-        Debug.DrawRay(frontVec, Vector2.left*randomVelocity, new Color(0, 0, 1));
-        RaycastHit2D rayHit2 = Physics2D.Raycast(transform.position,Vector2.left * randomVelocity, 2f, LayerMask.GetMask("Platform"));
-        
-        if (rayHit2.collider != null && boxCollider.enabled != false)//jump
+        Vector2 frontVec = new Vector2(transform.position.x - (randomVelocity * 0.64f), transform.position.y);
+        Debug.DrawRay(frontVec, Vector2.down * 2.0f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector2.down * 2f, 1, LayerMask.GetMask("Platform"));
+        RaycastHit2D anotherRayHit = Physics2D.Raycast(frontVec, Vector2.down * 2f, 1, LayerMask.GetMask("Wall"));
+        Debug.DrawRay(new Vector2(transform.position.x, transform.position.y + 0.1f), Vector2.left * randomVelocity, new Color(0, 0, 1));
+        RaycastHit2D rayHit2 = Physics2D.Raycast(transform.position, Vector2.left * randomVelocity, 1.0f, LayerMask.GetMask("Platform"));
+
+        RaycastHit2D checkPlatform = Physics2D.Raycast(new Vector2(transform.position.x - (randomVelocity * 0.64f), transform.position.y - 0.64f), Vector2.left * randomVelocity * (-1), 1.28f, LayerMask.GetMask("Wall"));
+        if (checkPlatform.collider != null && jumpTime < 1)
+            jumpTime = 1;  // 점프 횟수 초기화
+
+        if (rayHit2.collider != null && boxCollider.enabled != false && jumpTime > 0)//jump
         {
-           gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 0.2f, ForceMode2D.Impulse);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 0.6f, ForceMode2D.Impulse);
+            jumpTime -= 1;
         }
 
         float rayfrontDistance = 10f;
@@ -59,32 +68,41 @@ public class EnemyController : MonoBehaviour
         Debug.DrawRay(transup, Vector2.left * randomVelocity * raybackDistance * (-1), new Color(1, 0, 0));
         Debug.DrawRay(transdown, Vector2.left * randomVelocity * raybackDistance * (-1), new Color(1, 0, 0));
 
-        RaycastHit2D rayHitf1 = Physics2D.Raycast(transform.position, Vector3.left* randomVelocity, rayfrontDistance, LayerMask.GetMask("Player"));
-        RaycastHit2D rayHitf2 = Physics2D.Raycast(transup, Vector3.left* randomVelocity, rayfrontDistance, LayerMask.GetMask("Player"));
-        RaycastHit2D rayHitf3 = Physics2D.Raycast(transdown, Vector3.left* randomVelocity, rayfrontDistance, LayerMask.GetMask("Player"));
-        RaycastHit2D rayHitb1 = Physics2D.Raycast(transform.position, Vector3.left* randomVelocity * (-1), raybackDistance, LayerMask.GetMask("Player"));
-        RaycastHit2D rayHitb2 = Physics2D.Raycast(transup, Vector3.left* randomVelocity * (-1), raybackDistance, LayerMask.GetMask("Player"));
-        RaycastHit2D rayHitb3 = Physics2D.Raycast(transdown, Vector3.left* randomVelocity * (-1), raybackDistance, LayerMask.GetMask("Player"));
+        RaycastHit2D rayHitf1 = Physics2D.Raycast(transform.position, Vector3.left * randomVelocity, rayfrontDistance, LayerMask.GetMask("Player"));
+        RaycastHit2D rayHitf2 = Physics2D.Raycast(transup, Vector3.left * randomVelocity, rayfrontDistance, LayerMask.GetMask("Player"));
+        RaycastHit2D rayHitf3 = Physics2D.Raycast(transdown, Vector3.left * randomVelocity, rayfrontDistance, LayerMask.GetMask("Player"));
+        RaycastHit2D rayHitb1 = Physics2D.Raycast(transform.position, Vector3.left * randomVelocity * (-1), raybackDistance, LayerMask.GetMask("Player"));
+        RaycastHit2D rayHitb2 = Physics2D.Raycast(transup, Vector3.left * randomVelocity * (-1), raybackDistance, LayerMask.GetMask("Player"));
+        RaycastHit2D rayHitb3 = Physics2D.Raycast(transdown, Vector3.left * randomVelocity * (-1), raybackDistance, LayerMask.GetMask("Player"));
 
         if (rayHitf1.collider != null || rayHitf2.collider != null || rayHitf3.collider != null || rayHitb1.collider != null || rayHitb2.collider != null || rayHitb3.collider != null)
         {//if enemy find player
-            if (rayHitf1.collider != null || rayHitf2.collider != null || rayHitf3.collider != null){
+            if (rayHitf1.collider != null || rayHitf2.collider != null || rayHitf3.collider != null)
+            {
                 moveSpeed = 2f;
-                if (spriteRenderer.flipX == true){
+                if (spriteRenderer.flipX == false)
+                {
                     switchvalue = 1;
-                    rigid.velocity = new Vector2(Mathf.Abs(randomVelocity)* moveSpeed, rigid.velocity.y);
+
+                    if (!((Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.63f), Vector2.left * randomVelocity, 0.7f, LayerMask.GetMask("Platform"))) || (Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.63f), Vector2.left * randomVelocity, 0.7f, LayerMask.GetMask("Wall")))))
+                        rigid.velocity = new Vector2(Mathf.Abs(randomVelocity) * moveSpeed, rigid.velocity.y);
                 }
-                else if (spriteRenderer.flipX == false){
+                else if (spriteRenderer.flipX == true)
+                {
                     switchvalue = 2;
-                    rigid.velocity = new Vector2(Mathf.Abs(randomVelocity)* moveSpeed * (-1), rigid.velocity.y);
+
+                    if (!((Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.63f), Vector2.left * randomVelocity, 0.7f, LayerMask.GetMask("Platform"))) || (Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.63f), Vector2.left * randomVelocity, 0.7f, LayerMask.GetMask("Wall")))))
+                        rigid.velocity = new Vector2(Mathf.Abs(randomVelocity) * moveSpeed * (-1), rigid.velocity.y);
                 }
 
                 //FixRandomVelocity(true);
             }
-            else {
+            else
+            {
                 //FixRandomVelocity(false);
             }
-            if  (rayHitb1.collider != null || rayHitb2.collider != null || rayHitb3.collider != null) {// find player back, turn
+            if (rayHitb1.collider != null || rayHitb2.collider != null || rayHitb3.collider != null)
+            {// find player back, turn
                 Turn();
             }
             CancelInvoke();
@@ -105,6 +123,7 @@ public class EnemyController : MonoBehaviour
         {
             // Player not detected
             moveSpeed = 1f;
+            switchvalue = 0;
             if (playerDetected)
             {
                 playerDetected = false;
@@ -120,11 +139,11 @@ public class EnemyController : MonoBehaviour
         {
             Invoke("Turn", 1);
         }
-        if (!rayHit.collider)// if enemy go end of field
+        if (!rayHit.collider && !anotherRayHit && jumpTime > 0)// if enemy go end of field
         {
-            //Turn();
+            Turn();
         }
-        if (randomVelocity != 0) spriteRenderer.flipX = randomVelocity == -1;
+        if (randomVelocity != 0) spriteRenderer.flipX = randomVelocity != -1;
     }
 
     IEnumerator randomMove()
@@ -132,26 +151,26 @@ public class EnemyController : MonoBehaviour
         float interval = Random.Range(2f, 5f);
         WaitForSeconds wait = new WaitForSeconds(interval);
         while (true)
-    {
-        if (switchvalue == 0)
         {
-            // randomVelocity를 무작위로 설정 (고정되지 않음)
-            randomVelocity = Random.Range(-1, 2);
-            anim.SetInteger("walkSpeed", randomVelocity);
-        }
-        else if ( switchvalue == 1)
-        {
-            // randomVelocity를 원하는 고정값으로 설정
-            randomVelocity = -1; // 예시로 1로 고정
-            anim.SetInteger("walkSpeed", randomVelocity); // 애니메이션 업데이트
-        }
-        else if (switchvalue == 2)
-        {
-            randomVelocity = 1; // 예시로 1로 고정
-            anim.SetInteger("walkSpeed", randomVelocity); // 애니메이션 업데이트
-        }
+            if (switchvalue == 0)
+            {
+                // randomVelocity를 무작위로 설정 (고정되지 않음)
+                randomVelocity = Random.Range(-1, 2);
+                anim.SetInteger("walkSpeed", randomVelocity);
+            }
+            else if (switchvalue == 1)
+            {
+                // randomVelocity를 원하는 고정값으로 설정
+                randomVelocity = -1; // 예시로 1로 고정
+                anim.SetInteger("walkSpeed", randomVelocity); // 애니메이션 업데이트
+            }
+            else if (switchvalue == 2)
+            {
+                randomVelocity = 1; // 예시로 1로 고정
+                anim.SetInteger("walkSpeed", randomVelocity); // 애니메이션 업데이트
+            }
 
-        yield return wait;
+            yield return wait;
         }
     }
 
@@ -205,6 +224,6 @@ public class EnemyController : MonoBehaviour
 
     public void FixRandomVelocity(bool fix)
     {
-    isRandomVelocityFixed = fix;
+        isRandomVelocityFixed = fix;
     }
 }
